a927ba3ec67faae151b8994fc6e41f3857fafafa
zhoujingang <1253822097@qq.com>
2018-03-01 18:46:12 +0800

    食物列表完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item, index) in goods" class="menu-item" :key="index">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current': index === currentIndex}" :key="index" @click="selectMenu(index)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="(item, index) in goods" class="food-list" :key="index">
+        <li v-for="(item, index) in goods" class="food-list food-list-hook" :key="index">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="(food, index) in item.foods" class="food-item border-1px" :key="index">
@@ -22,12 +22,11 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">¥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -35,10 +34,14 @@
         </li>
       </ul>
     </div>
+    <shopcart></shopcart>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+import shopcart from '@/components/shopcart/shopcart'
+
 const ERR_OK = 0
 
 export default {
@@ -49,8 +52,25 @@ export default {
   },
   data () {
     return {
-      goods: {}
+      goods: {},
+      listHeight: [],
+      scrollY: 0
     }
+  },
+  computed: {
+    currentIndex () {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        }
+      }
+      return 0
+    }
+  },
+  components: {
+    shopcart
   },
   created () {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
@@ -58,8 +78,40 @@ export default {
       res = res.body
       if (res.erron === ERR_OK) {
         this.goods = res.data
+        this.$nextTick(() => {
+          this._initScroll()
+          this._calculateHeight()
+        })
       }
     })
+  },
+  methods: {
+    selectMenu (index) {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+      let el = foodList[index]
+      this.foodsScroll.scrollToElement(el, 300)
+    },
+    _initScroll () {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      })
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      })
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    _calculateHeight () {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    }
   }
 }
 </script>
@@ -83,6 +135,14 @@ export default {
         width: 56px
         line-height: 14px
         padding: 0 12px
+        &.current
+          position: relative
+          margin-top: -1px
+          z-index: 10
+          background: #ffffff
+          font-weight: 700
+          .text
+            border-none()
         .icon
           display: inline-block
           vertical-align: top
@@ -141,8 +201,9 @@ export default {
             color: rgb(147, 153, 159)
           .desc
             margin-bottom: 8px
+            line-height: 12px
           .extra
-            &.count
+            .count
               margin-right: 12px
           .price
             font-weight: 700
@@ -155,6 +216,5 @@ export default {
               text-decoration: line-through
               font-size: 10px
               color: rgb(147, 153, 159)
-
 
 </style>
