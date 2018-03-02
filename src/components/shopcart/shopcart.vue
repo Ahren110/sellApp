5c8b5c0d5623e7b480579b5e4fb9c56de5c2cb76
zhoujingang <1253822097@qq.com>
2018-03-02 18:33:19 +0800

    2018-3-2

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -4,11 +4,11 @@
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo">
-            <span class="icon-shopping_cart"></span>
+            <i class="icon-shopping_cart"></i>
           </div>
         </div>
-        <div class="price"></div>
-        <div class="desc"></div>
+        <div class="price">¥0</div>
+        <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
 
@@ -19,7 +19,21 @@
 
 <script>
 export default {
+  props: {
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
 
+    }
+  }
 }
 </script>
 
@@ -32,4 +46,54 @@ export default {
     width: 100%
     height: 48px
     background: #000
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            text-align: center
+            background: #2b343c
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          line-height: 24px
+          box-sizing: border-box
+          padding-right: 12px
+          border-right: 1px solid rgba(255, 255, 255, .1)
+          font-size: 16px
+          font-weight: 700
+          color: rgba(255, 255, 255, .4)
+        .desc
+          display: inline-block
+          vertical-align: top
+          margin: 12px 0 0 12px
+          line-height: 24px
+          font-size: 10px
+          color: rgba(255, 255, 255, .4)
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+
 </style>
